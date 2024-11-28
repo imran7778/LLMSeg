a4fda4b8035369a7d4712c8200566e9c653bcdff
Imran <31092568+imran7778@users.noreply.github.com>
2024-11-28 17:43:05 +0100

    Update prepare_ReasonSeg.py

--- prepare_datasets/prepare_ReasonSeg.py
+++ prepare_datasets/prepare_ReasonSeg.py
@@ -8,8 +8,8 @@ from pycocotools import mask as mask_utils
 from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
 
 
-dataset_root = "/cluster/scratch/leikel/junchi/lisa_dataset/reason_seg/ReasonSeg/"
-output_root = "/cluster/home/leikel/junchi/processed_data/reason_seg/ReasonSeg/"
+dataset_root = "/kaggle/working/LLMSeg/dataset/reason_seg/ReasonSeg/"
+output_root = "/kaggle/working/LLMSeg/process_dataset/reason_seg/ReasonSeg/"
 sam_checkpoint = "/cluster/home/leikel/junchi/segment-anything/checkpoints/sam_vit_h_4b8939.pth"
 
 
